64f8edf7ff026d8a5659fab5a505788c8333d827
felipe_N <felipe.novoa2020@twk.cl>
2020-12-20 01:42:54 -0300

    avance del dia 20-12-2020

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserSeeder::class);
+        // $this->call(UserSeeder::class);
         $this->call(ReservaSeeder::class);
-        $this->call(TipoMesaSeeder::class);
+        // $this->call(TipoMesaSeeder::class);
     }
 }
